6b54548b84d89634d646593e96539a544c16415a
NimcoMustapha <nimcomustapha@gmail.com>
2025-11-25 00:09:00 +0300

    App file

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+
+import NavBar from "./Component/NavBar";
+import Hero from "./Component/Hero";
+import About from "./Component/About";
+import Gallery from "./Component/Gallery";
+import ContactUs from "./Component/ContactUs";
+import Footer from "./Component/Footer";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Hero />
+      <About />
+      <Gallery />
+      <ContactUs />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
